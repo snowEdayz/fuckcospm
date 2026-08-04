72807629f8a06044adfbe433b539128be671cabd
snowEdayz <126792672+snowEdayz@users.noreply.github.com>
2026-08-04 16:16:00 +0800

    Update FuckCospmModule.java

--- app/src/main/java/com/fuckcospm/FuckCospmModule.java
+++ app/src/main/java/com/fuckcospm/FuckCospmModule.java
@@ -1,6 +1,10 @@
 package com.fuckcospm;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Binder;
+import android.os.UserHandle;
+import android.util.Pair;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
@@ -12,9 +16,7 @@ public class FuckCospmModule implements IXposedHookLoadPackage {
 
     private static final String TAG = "fuckcospm";
 
-    private static final int RESULT_NEED_CONFIRM = 0;
-
-    private static final String[] TARGET_CALLEE_PACKAGES = {
+    private static final String[] TARGET_PACKAGES = {
             "com.heytap.market",
             "com.nearme.instant.platform"
     };
@@ -24,28 +26,91 @@ public class FuckCospmModule implements IXposedHookLoadPackage {
         if (!"android".equals(lpparam.packageName)) {
             return;
         }
+        ClassLoader cl = lpparam.classLoader;
         try {
-            Class<?> whiteListClass = XposedHelpers.findClass(
-                    "com.android.server.am.OplusSecurityPermissionManager$ActivityStartWhiteList",
-                    lpparam.classLoader);
+            final Class<?> confirmManagerClass = XposedHelpers.findClass(
+                    "com.android.server.wm.OplusAppStartConfirmManager", cl);
+            final Class<?> activityRecordClass = XposedHelpers.findClass(
+                    "com.android.server.wm.ActivityRecord", cl);
+            final Class<?> activityOptionsClass = XposedHelpers.findClass(
+                    "android.app.ActivityOptions", cl);
+            final Class<?> profilerInfoClass = XposedHelpers.findClass(
+                    "android.app.ProfilerInfo", cl);
+
             XposedHelpers.findAndHookMethod(
-                    whiteListClass,
-                    "checkAllowStartActivity",
-                    String.class, String.class, Intent.class, int.class, int.class,
+                    confirmManagerClass,
+                    "checkStartActivityForConfirm",
+                    activityRecordClass,
+                    ActivityInfo.class,
+                    Intent.class,
+                    int.class,
+                    int.class,
+                    String.class,
+                    activityOptionsClass,
+                    profilerInfoClass,
+                    boolean.class,
                     new XC_MethodHook() {
                         @Override
                         protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                            String calleePkg = (String) param.args[1];
-                            if (isTarget(calleePkg)) {
-                                XposedBridge.log(TAG + ": whitelist bypassed, callerPkg=" + param.args[0]
-                                        + ", calleePkg=" + calleePkg);
-                                param.setResult(RESULT_NEED_CONFIRM);
+                            if (param.args[1] == null || param.args[2] == null) {
+                                return;
+                            }
+                            Object aInfoApp = XposedHelpers.getObjectField(param.args[1], "applicationInfo");
+                            if (aInfoApp == null) {
+                                return;
+                            }
+                            String targetPkg = (String) XposedHelpers.getObjectField(aInfoApp, "packageName");
+                            if (!isTarget(targetPkg)) {
+                                return;
+                            }
+                            XposedBridge.log(TAG + ": forcing confirm dialog, calleePkg=" + targetPkg
+                                    + ", callerPkg=" + param.args[5]);
+                            try {
+                                Object manager = param.thisObject;
+                                String callerPkg = (String) param.args[5];
+                                int callerUid = ((Integer) param.args[4]).intValue();
+                                int calleeUid = ((Integer) XposedHelpers.getObjectField(aInfoApp, "uid")).intValue();
+                                int userId = UserHandle.getUserId(calleeUid);
+
+                                Intent confirmIntent = (Intent) XposedHelpers.callMethod(
+                                        manager,
+                                        "getCheckConformIntent",
+                                        callerPkg,
+                                        param.args[1],
+                                        param.args[2],
+                                        param.args[3],
+                                        Integer.valueOf(callerUid),
+                                        param.args[0],
+                                        Integer.valueOf(userId),
+                                        Integer.valueOf(0),
+                                        param.args[6]);
+
+                                Object mAtms = XposedHelpers.getObjectField(manager, "mAtms");
+                                Object supervisor = XposedHelpers.getObjectField(mAtms, "mTaskSupervisor");
+                                Object resolved = XposedHelpers.callMethod(
+                                        supervisor,
+                                        "resolveActivity",
+                                        confirmIntent,
+                                        null,
+                                        Integer.valueOf(0),
+                                        param.args[7],
+                                        Integer.valueOf(UserHandle.getUserId(callerUid)),
+                                        Integer.valueOf(callerUid),
+                                        Integer.valueOf(Binder.getCallingUid()));
+                                if (resolved != null) {
+                                    param.setResult(new Pair<>(new Pair<>(confirmIntent, resolved), Boolean.FALSE));
+                                } else {
+                                    XposedHelpers.setObjectField(manager, "mHasConformActivity", Boolean.FALSE);
+                                    param.setResult(null);
+                                }
+                            } catch (Throwable t) {
+                                XposedBridge.log(TAG + ": forced confirm failed: " + t);
                             }
                         }
                     });
             XposedBridge.log(TAG + ": hook installed");
         } catch (Throwable t) {
-            XposedBridge.log(TAG + ": hook failed: " + t);
+            XposedBridge.log(TAG + ": hook install failed: " + t);
         }
     }
 
@@ -53,7 +118,7 @@ public class FuckCospmModule implements IXposedHookLoadPackage {
         if (calleePkg == null) {
             return false;
         }
-        for (String pkg : TARGET_CALLEE_PACKAGES) {
+        for (String pkg : TARGET_PACKAGES) {
             if (pkg.equals(calleePkg)) {
                 return true;
             }
